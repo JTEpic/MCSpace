2ca4437cde7b3cab8e784cf8097f05fc99eb792a
JTEpic <jjrocks2006@att.net>
2025-10-14 15:31:36 -0500

    init

---
+++ src/client/java/com/jtepic/mcspace/MCSpaceClient.java
@@ -0,0 +1,10 @@
+package com.jtepic.mcspace;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCSpaceClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/jtepic/mcspace/item/ModItems.java
@@ -0,0 +1,32 @@
+package com.jtepic.mcspace.item;
+
+import com.jtepic.mcspace.MCSpace;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ModItems {
+    public static final Item PARACHUTE = registerItem("parachute", Item::new);
+    public static final Item GREEN_PARACHUTE = registerItem("green_parachute", Item::new);
+
+    private static Item registerItem(String name, Function<Item.Settings, Item> function) {
+        return Registry.register(Registries.ITEM, Identifier.of(MCSpace.MOD_ID, name),
+                function.apply(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MCSpace.MOD_ID, name)))));
+    }
+
+    public static void registerModItems() {
+        MCSpace.LOGGER.info("Registering Mod Items for " + MCSpace.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PARACHUTE);
+            entries.add(GREEN_PARACHUTE);
+        });
+    }
+}
